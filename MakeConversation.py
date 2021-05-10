bbb55d1eeb117d6531b840e65a29d8671724a617
Ankun <sytungan@gmail.com>
2021-05-11 00:10:05 +0700

    Add login function

--- MakeConversation.py
+++ MakeConversation.py
@@ -1,6 +1,7 @@
 from platform import platform
 import selenium
 from platform import system 
+import time
 from selenium import webdriver
 print(system())
 if system() == 'Windows':
@@ -10,7 +11,16 @@ elif system() == 'Linux':
 elif system() == 'Darwin':
     PATH = "./chrome_driver/chromedriver_mac64/chromedriver"
 dest = open("chrome_driver/notes.txt")
-driver = webdriver.Chrome(PATH)
-driver.get("https://www.tinhte.vn")
+chrome_options = webdriver.ChromeOptions()
+chrome_options.add_argument("./Cookies")
+driver = webdriver.Chrome(PATH, chrome_options=chrome_options)
+driver.get("https://tinhte.vn/login")
 print(driver.title)
+# driver.find_element_by_class_name("fbLogin").click()
+login = driver.find_elements_by_name("login")
+login[2].send_keys("test")
+button = driver.find_elements_by_class_name("button.primary")
+button[3].click()
+time.sleep(30)
+
 driver.quit()
