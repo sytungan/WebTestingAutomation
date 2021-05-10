4996f1903ed195926ae77c4bd0690a34de007f9a
Ankun <sytungan@gmail.com>
2021-05-10 17:12:58 +0700

    Add simple search test

--- Search.py
+++ Search.py
@@ -1,7 +1,10 @@
 from platform import platform
 import selenium
+import time
 from platform import system 
 from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
+
 print(system())
 if system() == 'Windows':
     PATH = "./chrome_driver/chromedriver_win32/chromedriver.exe"
@@ -13,4 +16,14 @@ dest = open("chrome_driver/notes.txt")
 driver = webdriver.Chrome(PATH)
 driver.get("https://www.tinhte.vn")
 print(driver.title)
+
+
+searchButton = driver.find_element_by_class_name("placeholder")
+searchButton.click()
+
+searchTextBox = driver.find_element_by_class_name("search-textbox")
+searchTextBox.send_keys("test")
+searchTextBox.send_keys(Keys.RETURN)
+print(searchButton)
+time.sleep(5)
 driver.quit()
